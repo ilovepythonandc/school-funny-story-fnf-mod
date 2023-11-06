3d56d473a2e5c5ebbb7e00a5e850e4d1d86d0746
laiabao <141012900+ilovepythonandc@users.noreply.github.com>
2023-11-06 23:25:30 +0800

    file

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -10,7 +10,6 @@ class HealthIcon extends FlxSprite
 	public function new(char:String = 'bf', isPlayer:Bool = false, ?allowGPU:Bool = true)
 	{
 		super();
-		isOldIcon = (char == 'bf-old');
 		this.isPlayer = isPlayer;
 		changeIcon(char, allowGPU);
 		scrollFactor.set();
@@ -32,12 +31,12 @@ class HealthIcon extends FlxSprite
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
 			
 			var graphic = Paths.image(name, allowGPU);
-			loadGraphic(graphic, true, Math.floor(graphic.width / 2), Math.floor(graphic.height));
-			iconOffsets[0] = (width - 150) / 2;
-			iconOffsets[1] = (height - 150) / 2;
+			loadGraphic(graphic, true, Math.floor(graphic.width / 3), Math.floor(graphic.height));
+			iconOffsets[0] = (width - 150) / 3;
+			iconOffsets[1] = (height - 150) / 3;
 			updateHitbox();
 
-			animation.add(char, [0, 1], 0, false, isPlayer);
+			animation.add(char, [0, 1, 2], 0, false, isPlayer);
 			animation.play(char);
 			this.char = char;
 
